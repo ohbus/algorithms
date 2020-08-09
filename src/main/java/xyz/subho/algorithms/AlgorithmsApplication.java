0776bcbf0fe3004c81313b0ce9827cfb15da6c26
Subhrodip Mohanta <contact@subho.xyz>
2020-08-09 09:00:10 +0530

    initial commit

---
+++ src/main/java/xyz/subho/algorithms/AlgorithmsApplication.java
@@ -0,0 +1,12 @@
+package xyz.subho.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlgorithmsApplication.class, args);
+  }
+}
---
+++ src/test/java/xyz/subho/algorithms/AlgorithmsApplicationTests.java
@@ -0,0 +1,11 @@
+package xyz.subho.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
