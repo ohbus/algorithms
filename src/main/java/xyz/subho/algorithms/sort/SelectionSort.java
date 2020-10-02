8015a3374071607aa7f8024d8fef4f929d262cb2
SUBHADEEP MAITI <35770307+deepsubha@users.noreply.github.com>
2020-10-02 23:41:08 +0530

    selection sort

--- src/main/java/xyz/subho/algorithms/sort/SelectionSort.java
+++ src/main/java/xyz/subho/algorithms/sort/SelectionSort.java
@@ -1,25 +1,19 @@
-class SelectionSort{
-    static void selectionSort(int arr[], int n)                         
-    {  
-        int i, j, minimum;  
-        for (i = 0; i < n-1; i++)  
-        {  
-            minimum = i;  
-            for (j = i+1; j < n; j++)  
-            if (arr[j] < arr[minimum])  
-                minimum = j;   
-            int temp = arr[minimum];
-            arr[minimum]=arr[i];
-            arr[i] = temp;
-        } 
-        for(int eachElement:arr)                                        //printing sorted array
-            System.out.print(eachElement+" "); 
+class SelectionSort {
+  static void selectionSort(int arr[], int n) {
+    int i, j, minimum;
+    for (i = 0; i < n - 1; i++) {
+      minimum = i;
+      for (j = i + 1; j < n; j++) if (arr[j] < arr[minimum]) minimum = j;
+      int temp = arr[minimum];
+      arr[minimum] = arr[i];
+      arr[i] = temp;
     }
+    for (int eachElement : arr) // printing sorted array
+    System.out.print(eachElement + " ");
+  }
 
-
-    public static void main(String[] a){
-        int[] unsortedArray = new int[]{8,2,9,4,5,1};                   //hardcoded array
-        selectionSort(unsortedArray,unsortedArray.length);              //sending array with length
-    }
+  public static void main(String[] a) {
+    int[] unsortedArray = new int[] {8, 2, 9, 4, 5, 1}; // hardcoded array
+    selectionSort(unsortedArray, unsortedArray.length); // sending array with length
+  }
 }
-
