ccbfd8e15292f9b4bfc4e48a86aefaaae977881c
Souptik Hari <souptik43.hari@gmail.com>
2020-10-03 18:08:38 +0530

    Binary Search Update

--- src/main/java/xyz/subho/algorithms/search/BinarySearch.java
+++ src/main/java/xyz/subho/algorithms/search/BinarySearch.java
@@ -5,10 +5,13 @@ class BinarySearchExample {
     int mid = (first + last) / 2;
     while (first <= last) {
       if (arr[mid] < key) first = mid + 1;
-      else if (arr[mid] == key) {
+      else if (arr[mid] == key) 
+      {
         System.out.println(key + " is found at index: " + mid);
         break;
-      } else {
+      } 
+      else 
+      {
         last = mid - 1;
       }
       mid = (first + last) / 2;
@@ -17,13 +20,9 @@ class BinarySearchExample {
   }
 
   public static void main(String args[]) {
-    // Array provided to perform Binary Search should be ordered
-    int arr[] = {10, 20, 30, 40, 50};
-    // Key is the element to be found
-    int key = 30;
+    int arr[] = {10, 20, 30, 40, 50};// Array provided to perform Binary Search should be ordered
+    int key = 30;// Key is the element to be found
     int last = arr.length - 1;
-    // call the binarysearch funtion by passing the array , starting and endin index and key as
-    // parameter
-    binarySearch(arr, 0, last, key);
+    binarySearch(arr, 0, last, key);// call the binarysearch funtion by passing the array , starting and ending index and key
   }
 }
